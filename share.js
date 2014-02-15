f433d39359c74ccd7afba7a50b83e3543511b3db
Scott Sanders <scott@streetlinemedia.com>
2014-02-15 21:55:39 +1100

    Added dummy count and experimenting with various button styles

--- share.js
+++ share.js
@@ -4,58 +4,66 @@
  
     $.fn.share = function (options) {
  
-        var defaults = {
-            url: (typeof this.data('url') !== 'undefined') ? this.data('url') : document.URL,
-            via: (typeof this.data('via') !== 'undefined') ? this.data('via') : false,
-            popup: (typeof this.data('popup') !== 'undefined') ? this.data('popup') : true,
-            networks: ['facebook','twitter']
-        }
+        var defaults = {}
 
         var $this = this,
             settings = $.extend(defaults, options);
 
         var init = function($element) {
-            
-            var $wrapper = $('<ul class="js-share-wrapper" />');
-            
 
-            var index;
-            for (index = 0; index < settings.networks.length; ++index) {
-                
-                var $network = $('<a class="js-share-network">'),
-                    $li = $('<li/>');
+            var options = {
+                url: (typeof $element.data('url') !== 'undefined') ? $element.data('url') : document.URL,
+                message: (typeof $element.data('message') !== 'undefined') ? $element.data('message') : false,
+                via: (typeof $element.data('via') !== 'undefined') ? $element.data('via') : false,
+                popup: (typeof $element.data('popup') !== 'undefined') ? $element.data('popup') : true,
+                network: (typeof $element.data('share') !== 'undefined') ? $element.data('share') : "facebook",
+            },
+                $count = $('<span class="js-share-count"/>');                        
 
-                if (settings.popup){
-                   
-                    $network.attr({
-                        rel:"external",
-                        target:"_blank"
-                    });
+            $element.addClass("js-share");
 
-                }
-
-                switch(settings.networks[index]){
-
-                    case 'facebook':
-                        $network.addClass("js-share-facebook");
-                        $network.text("Share this on Facebook").attr("href",settings.url);
-                        break;
-
-                    case 'twitter':
-                        $network.addClass("js-share-twitter");
-                        $network.text("Tweet about this").attr("href",settings.url);
-                       
-                        break;
-
-
-                }
-                $network.appendTo($li);
-                $li.appendTo($wrapper);
-
+            if (options.popup){
+               
+                $element.attr({
+                    rel:"external",
+                    target:"_blank"
+                });
 
             }
 
-            $element.before($wrapper).remove();
+            switch(options.network){
+
+                case 'facebook':
+                    
+                    var url = 'http://www.facebook.com/sharer/sharer.php?';
+                    if (options.url) url += 'u='+encodeURIComponent(options.url);
+
+                    $element
+                        .addClass("js-share-facebook")
+                        .attr("href",url);
+
+                    $count.appendTo($element).addClass("js-share-count-loading").load("facebook.json",function(data){
+                        $(this).text(data).removeClass('js-share-count-loading');
+                    });
+                    break;
+
+                case 'twitter':
+                    
+                    var url = 'https://twitter.com/intent/tweet?';
+                    if (options.via) url += 'via='+encodeURIComponent(options.via);
+                    if (options.message) url += 'text='+encodeURIComponent(options.message); //TODO: check if amperstand
+
+                    $element
+                        .addClass("js-share-twitter")
+                        .attr("href",url);
+
+                    $count.appendTo($element).addClass("js-share-count-loading").load("twitter.json",function(data){
+                        $(this).text(data).removeClass('js-share-count-loading');
+                    });
+                    break;
+
+
+            }
 
         };
  
